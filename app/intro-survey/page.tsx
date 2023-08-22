fbd23420482924a6b28bbb8b0465cd7fdbc86efe
Luchohak <a00958934@gmail.com>
2023-08-22 17:11:15 -0600

    Start adding styles to GetStarted, Name and Age questions

--- app/intro-survey/page.tsx
+++ app/intro-survey/page.tsx
@@ -8,47 +8,65 @@ import NameSurvey from "./steps/NameSurvey";
 import SexSurvey from "./steps/SexSurvey";
 import GoalsSurvey from "./steps/GoalsSurvey";
 import ExperienceSurvey from "./steps/ExperienceSurvey";
+import Button from "../components/UI/Button";
+import GetStartedSurvey from "./steps/GetStartedSurvey";
 
 const SurveyPage = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(0);
 
   const nextStepHandler = () => {
-    if(step < 7)
-    setStep(step + 1);
+    if (step < 7) setStep(step + 1);
   };
 
   const previousStepHandler = () => {
-    if (step > 1)
-    setStep(step - 1);
+    if (step > 0) setStep(step - 1);
   };
 
   const stepManager = () => {
     switch (step) {
-      case 1: 
-        return <NameSurvey />
+      case 1:
+        return <NameSurvey />;
       case 2:
         return <AgeSurvey />;
       case 3:
         return <HeightSurvey />;
       case 4:
         return <WeightSurvey />;
-      case 5: 
+      case 5:
         return <SexSurvey />;
-      case 6: 
-        return <GoalsSurvey />
-      case 7: 
-      return <ExperienceSurvey />
+      case 6:
+        return <GoalsSurvey />;
+      case 7:
+        return <ExperienceSurvey />;
       default:
-        return <p>Get started</p>;
+        return (
+          <GetStartedSurvey
+            onStart={nextStepHandler}
+            buttonLabel="Start survey"
+          />
+        );
     }
   };
 
   return (
-    <section>
-      <h1>wassup babygirl</h1>
+    <section className="w-1/2">
       {stepManager()}
-      <button onClick={previousStepHandler}>Previous</button>
-      <button onClick={nextStepHandler}>Next</button>
+      {step === 0 ? (
+        ""
+      ) : (
+        <div className="flex items-center justify-between mt-12">
+          <Button
+            className="w-1/4"
+            onClick={previousStepHandler}
+            label="Previous"
+          />
+          <Button
+            className="w-1/4"
+            onClick={nextStepHandler}
+            label="Next"
+          />
+        </div>
+      )}
     </section>
   );
 };
---
+++ app/intro-survey/steps/GetStartedSurvey.tsx
@@ -0,0 +1,24 @@
+import Button from "../../components/UI/Button";
+
+interface GetStartedProps {
+  onStart: () => void;
+  buttonLabel: string;
+}
+
+const GetStartedSurvey: React.FC<GetStartedProps> = ({
+  onStart,
+  buttonLabel,
+}) => {
+  return (
+    <div className="flex items-center flex-col">
+      <div>
+        <h1 className="text-3xl my-4">help your coach get to know you better</h1>
+      </div>
+      <div>
+        <Button className="" onClick={onStart} label={buttonLabel} />
+      </div>
+    </div>
+  );
+};
+
+export default GetStartedSurvey;
