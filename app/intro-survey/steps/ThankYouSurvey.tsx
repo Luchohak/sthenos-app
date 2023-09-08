2382feb646455878e4930cfc305e36ccf90202b1
Luchohak <a00958934@gmail.com>
2023-09-08 16:44:17 -0600

    Use Link for profile component instead of an anchor tag to preserve Redux

--- app/intro-survey/steps/ThankYouSurvey.tsx
+++ app/intro-survey/steps/ThankYouSurvey.tsx
@@ -2,16 +2,22 @@ import Link from "next/link";
 
 const ThankYouSurvey = () => {
   return (
-    <div className="flex flex-col items-center text-center">
-      <h1 className="text-3xl my-4">thanks!</h1>
-      <p className="text-2xl">
-        this will help your coach align your workouts with your goals and experience
-      </p>
+    <div className="flex flex-col items-center text-center gap-6">
+      <div className="rounded bg-slate-400 w-1/2 p-4 text-slate-950 drop-shadow-2xl min-w-42">
+        <h1 className="text-3xl my-4">thanks!</h1>
+        <p className="text-2xl">
+          this helps a lot
+        </p>
+      </div>
+      <div className="rounded bg-slate-400 w-1/2 p-4 text-slate-950 drop-shadow-2xl">
+        <h1 className="text-3xl my-4">one last thingy!</h1>
+        <p className="text-2xl">sign up so we can complete your setup</p>
+      </div>
       <Link
-        href="/"
-        className="rounded my-10 py-2 px-4 bg-slate-200 text-slate-900 hover:bg-slate-400 hover:text-slate-800"
+        href="api/auth/login"
+        className="rounded text-lg font-bold flex items-center justify-center w-32 h-16 py-2 px-4 bg-slate-100 text-slate-900 hover:bg-amber-700 hover:text-slate-100"
       >
-        my profile
+        sign up
       </Link>
     </div>
   );
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { useEffect } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { useSelector } from "react-redux";
+import { ageSurveySelector } from "../redux/selectors";
 
 export default function ProfilePage() {
   const { user, error, isLoading } = useUser();
+  const userAge = useSelector(ageSurveySelector);
+
+  useEffect(() => {
+    console.log("age", userAge);
+  }, [userAge]);
 
   if (isLoading) return <div>Retrieving your info, almost there...</div>;
   if (error) return <div>{error.message}</div>;
@@ -13,7 +21,11 @@ export default function ProfilePage() {
       <div className="flex justify-center items-center flex-col">
         {user.picture && user.name ? (
           <>
-            <img className="rounded-full w-20 m-4" src={user.picture} alt={user.name}></img>
+            <img
+              className="rounded-full w-20 m-4"
+              src={user.picture}
+              alt={user.name}
+            ></img>
             <h2>Welcome {user.name}</h2>
             <p>{user.email}</p>
             <a href="/api/auth/logout">Logout</a>
